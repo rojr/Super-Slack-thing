67ce24f1b8420af6c517029e51b91203623da137
chee <chee@snaek.org>
2017-06-21 21:51:07 +0100

    add node integration (safely) to webview process

--- src/inject/index.js
+++ src/inject/index.js
@@ -1,1 +1,19 @@
-alert('i am inject')
+global.node = {
+  require: global.require,
+  process: global.process
+}
+
+delete global.require
+delete global.module
+delete global.__filename
+delete global.__dirname
+delete global.process
+
+const ipc = require('./ipc')
+
+document.addEventListener('readystatechange', function stateChange () {
+  if (document.readyState === 'complete') {
+    document.removeEventListener('readystatechange', stateChange)
+    ipc()
+  }
+})
