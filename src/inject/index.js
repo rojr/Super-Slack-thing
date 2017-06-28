2a05a06ed5d331198ab920589c360e939449a630
chee <chee@snaek.org>
2017-06-28 05:47:24 +0100

    require modules outside, then call them after load
    providing they export a function

--- src/inject/index.js
+++ src/inject/index.js
@@ -6,9 +6,14 @@ delete global.__filename
 delete global.__dirname
 delete global.process
 
+const modules = [
+  require('./ipc'),
+  require('./ui')
+]
+
 document.addEventListener('readystatechange', function stateChange () {
   if (document.readyState === 'complete') {
+    modules.forEach(module => typeof module === 'function' && module(electron))
     document.removeEventListener('readystatechange', stateChange)
-    require('./ipc')(electron)
   }
 })
